cc18a26a52f9821e7bd8185b7d69eb94d913b890
Rafah <RafahFilfilan@gmail.com>
2020-10-27 17:11:37 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 const header = document.querySelector("header");
 const banner = document.querySelector("#banner");
 
+const navEle = document.createElement('nav');
+const ulEle = document.createElement('ul');
+
+header.appendChild(navEle);
+navEle.setAttribute("id", "hd");
+navEle.appendChild(ulEle);
+ulEle.setAttribute("id", "navList");
+
+const sections = document.querySelectorAll('section a');
+
+buildMenu(sections);
+
+
+//Build the <li> elements
+function buildMenu (liElements) {
+	liElements.forEach((ele, index) => {
+		const liEle = document.createElement('li');
+		liEle.classList.add("navItem");
+		liEle.innerHTML = `<a id=${index} onclick=theySeeMeScrolling('${ele.id}');>${ele.name}</a>`;
+		ulEle.appendChild(liEle);
+	});
+}
+
+
+//To scroll when navbar is clicked
+function theySeeMeScrolling(id){
+	const theyLovin = document.getElementById(id);
+	console.log(theyLovin);
+	theyLovin.scrollIntoView();
+}
+
+
+
 //Check the margin of transitioning.
 const bannerOptions = {
 	rootMargin: "-200px 0px 0px 0px "
@@ -14,7 +47,6 @@ const bannerObserver = new IntersectionObserver(function(
 	entries.forEach(entry => {
 		(!entry.isIntersecting) ? header.classList.add("nav-scrolled") : header.classList.remove("nav-scrolled");
 	})
-	
 }, bannerOptions);
 
 bannerObserver.observe(banner);
@@ -22,8 +54,6 @@ bannerObserver.observe(banner);
 
 
 //Active navigation on Scroll
-const sections = document.querySelectorAll('section a');
-
 onscroll = function (){
 	const scrollPosition = document.documentElement.scrollTop;
 	
@@ -57,7 +87,21 @@ const removeAllActiveClassesFromTitles = function (){
 };
 
 const addActiveClassToLink = function(id){
-	const selectLink = `nav ul li a[href="#${id}"]`;
+	let selectLink;
+	switch (id) {
+		case sections[0].id:
+		selectLink = `nav ul li a[id="0"]`;
+		break;
+		case sections[1].id:
+		selectLink = `nav ul li a[id="1"]`;
+		break;
+		case sections[2].id:
+		selectLink = `nav ul li a[id="2"]`;
+		break;
+		case sections[3].id:
+		selectLink = `nav ul li a[id="3"]`;
+		break;
+	} 
 	document.querySelector(selectLink).classList.add("activeItem");
 };
 
@@ -65,6 +109,3 @@ const addActiveClassToTitle = function(id){
 	const selectTitle = document.getElementById(id);
 	selectTitle.classList.add("activeItem");
 };
-
-
-
